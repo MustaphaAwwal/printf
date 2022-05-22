8faea7d77aca5c7fe3248542110bbfc4a178cc1b
bello oluwatobi <61856590+oluwakore@users.noreply.github.com>
2022-05-22 15:56:39 +0100

    Update main.h

--- main.h
+++ main.h
@@ -7,6 +7,21 @@
 #include <limits.h>
 #include <stdlib.h>
 
+
+/* Struct */
+
+/**
+ *struct setter - structure for printing types
+ *@typ: type to print
+ *@f: function to print
+ */
+
+typedef struct setter
+{
+        const char typ;
+        int (*f)(va_list);
+} setter_t;
+
 /* prototype for _prinf.c*/
 int _printf(const char *format, ...);
 int _putchar(char a);
